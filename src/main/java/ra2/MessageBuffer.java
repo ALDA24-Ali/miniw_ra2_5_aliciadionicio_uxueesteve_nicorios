f4bcaf8d742029419201179bd7eb4e9475e71439
Alicia Dionicio <aldiaz24@bemen3.cat>
2025-11-25 17:21:31 +0100

    guardado1

---
+++ src/main/java/ra2/MessageBuffer.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MessageBuffer {
+
+    private final Queue<String> buffer = new LinkedList<>();
+
+    // Método para añadir mensajes al buffer
+    public synchronized void put(String msg) {
+        buffer.add(msg);
+        notifyAll(); // Avisamos a los hilos que estén esperando
+    }
+
+    // Método para extraer mensajes del buffer
+    public synchronized String take() throws InterruptedException {
+        while (buffer.isEmpty()) {
+            wait(); // Esperamos hasta que alguien ponga un mensaje
+        }
+        return buffer.poll();
+    }
+}
---
+++ src/main/java/ra2/sockets.java
@@ -0,0 +1,5 @@
+package ra2;
+
+public class sockets {
+    
+}
